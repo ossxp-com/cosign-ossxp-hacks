b1b1619664abd7d3caa8b69c49fa2a1e24e8845f
Jiang Xin <worldhello.net AT gmail DOT com>
2008-12-03 14:55:29 +0000

    clean cookie

--- debian/sites/site1/www/logout/index.php
+++ debian/sites/site1/www/logout/index.php
@@ -7,8 +7,8 @@ require("../include.inc.php");
  * make any local additions here (e.g. expiring local sessions, etc.),
  * but it's important that there be no output on this page.
  */
-#setcookie( "cosign-site1", "null", time()-1, '/', "", 1 );
-#setcookie( "cosign-service", "null", time()-1, '/', "", 1 );
+setcookie( "cosign-site1", "null", time()-1, '/', "", 1 );
+setcookie( "cosign-service", "null", time()-1, '/', "", 1 );
 setcookie( $_SERVER['COSIGN_SERVICE'], "null", time()-1, '/', "", 1 );
 
 /*

0250ac32684f1caa0342845d254cfec7733b3875
Jiang Xin <worldhello.net AT gmail DOT com>
2008-12-03 04:59:04 +0000

    add example sites

---
+++ debian/sites/site1/www/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$username = $_SERVER["REMOTE_USER"];
+if ($username)
+    print "Welcome ".$username;
+else
+    print "Hello, Anonymous";
+
+?>
+<hr>
+
+<ul>
+<li> <a href="https://weblogin.local/cosign-bin/logout">logout</a></li> <br />
+	    When you are finished using all authenticated web resources,
+	    be sure to logout.
+<li> cookie: <?php echo $_SERVER["HTTP_COOKIE"]; ?>
+</ul>
+
+<?php
+phpinfo();
+?>

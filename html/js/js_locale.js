33fc7546ee4b9095a01fe65c01ef868afc3402ca
Jiang <jiangxin@ossxp.com>
2010-03-30 16:22:24 +0800

    message translated using gettext, and javascript no used.

--- html/js/js_locale.js
+++ html/js/js_locale.js
@@ -1,35 +1,7 @@
 var Localization = {
-	"0001": {
-		"en": "Login failed: user does not exist or wrong password.",
-		"zh": "登录失败: 用户不存在或错误的口令。" },
-
-	"0002": {
-		"en": "Cannot connect to LDAP server.",
-		"zh": "无法连接到 LDAP 服务器。" },
-
-	"0101": {
-		"en": "Login failed: email or invite_code not provided.",
-		"zh": "登录失败: 请输入邮件地址以及邀请码。" },
-
-	"0102": {
-		"en": "Login failed: invite factor has not setup yet.",
-		"zh": "登录失败: 邀请码尚未设置。" },
-
-	"0103": {
-		"en": "Login failed: provide your email address as login name.",
-		"zh": "登录失败: 请在用户名输入框输入您的邮件地址。" },
-
-	"0104": {
-		"en": "Login failed: wrong invite code.",
-		"zh": "登录失败: 错误的邀请码。" },
-
-	"0105": {
-		"en": "Please enter your login and password.",
-		"zh": "请输入您的用户名和口令。" },
-
-	"0106": {
-		"en": "Please input your email address into username field.",
-		"zh": "请在用户名输入框输入您的邮件地址。" },
+	"9999": {
+		"en": "Cannot open logfile.",
+		"zh": "无法打开日志文件。" },
 
 	"default": "en"
 };
